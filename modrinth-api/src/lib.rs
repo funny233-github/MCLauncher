844d18d0d94e814515b69c1ab2e1af8207b91780
funny_233 <83470506+funny233-github@users.noreply.github.com>
2025-02-16 16:07:59 +0800

    Search mod (#13)
    * feat(modrinth-api): Added the function of obtaining related Projects
    
    * feat(modrinth_api): Added asynchronous fetching for Projects
    
    The asynchronous fetching function is added to the Projects part of the modrinth_api,
    which helps to improve the efficiency of data retrieval
    in the project and avoid blocking the main thread.
    
    * docs(modrinth-api): Complement Doc Test for asynchronous functions
    
    The Doc Test for asynchronous functions has been supplemented,
    making the test examples in the document more comprehensive and accurate.
    
    * feat[modmanage, modrinth api]: Add mod search function.
    
    * fix[modrinth-api]: Correct spelling error in is_support_loader method
    
    Fixed the spelling error in the is_support_loader method by
    changing is_supoprt_loader to is_support_loader to ensure the correctness of the method name.
    
    * refactor [modmanage]: correct error handling for fetch_blocking in search function
    
    * docs(README): add description of `launcher mod search` (#12)

--- modrinth-api/src/lib.rs
+++ modrinth-api/src/lib.rs
@@ -26,6 +26,16 @@ pub struct Version {
     pub files: Vec<VersionFile>,
 }
 
+impl Version {
+    pub fn is_support_game_version(&self, game_version: &str) -> bool {
+        self.game_versions.iter().any(|v| v == game_version)
+    }
+
+    pub fn is_support_loader(&self, game_loader: &str) -> bool {
+        self.loaders.iter().any(|l| l == game_loader)
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Versions {}
 
@@ -39,6 +49,7 @@ impl Versions {
     /// ```
     pub fn fetch_blocking(slug: &str) -> Result<Vec<Version>> {
         let client = Client::new();
+        let mut err_detail = None;
         for _ in 0..5 {
             let init = client
                 .get(format!(
@@ -56,18 +67,35 @@ impl Versions {
                 std::thread::sleep(Duration::from_secs(3));
                 continue;
             };
-            if let Ok(_json) = send.json() {
-                return Ok(_json);
-            } else {
-                continue;
-            }
+            match send.json() {
+                Ok(_json) => return Ok(_json),
+                Err(e) => {
+                    err_detail = Some(e);
+                    continue;
+                }
+            };
         }
 
-        Err(anyhow::anyhow!("modrinth Versions fetch timeout!"))
+        Err(anyhow::anyhow!(
+            "modrinth Versions fetch timeout!\ndetail:{:#?}",
+            err_detail
+        ))
     }
 
+    /// Fetches the list of verseions for a project with the given slug
+    /// # Examples
+    /// ```
+    /// use modrinth_api::Versions;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let versions = Versions::fetch("fabric-api").await.unwrap();
+    ///     assert!(versions.len() > 0);
+    /// }
+    /// ```
     pub async fn fetch(slug: &str) -> Result<Vec<Version>> {
         let client = reqwest::Client::new();
+        let mut err_detail = None;
         for _ in 0..5 {
             let init = client
                 .get(format!(
@@ -85,13 +113,164 @@ impl Versions {
                 tokio::time::sleep(Duration::from_secs(3)).await;
                 continue;
             };
-            if let Ok(_json) = send.json().await {
-                return Ok(_json);
+            match send.json().await {
+                Ok(_json) => return Ok(_json),
+                Err(e) => {
+                    err_detail = Some(e);
+                    continue;
+                }
+            };
+        }
+
+        Err(anyhow::anyhow!(
+            "modrinth Versions fetch timeout!\ndetail:{:#?}",
+            err_detail
+        ))
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Hit {
+    pub project_id: String,
+    pub project_type: String,
+    pub slug: String,
+    pub author: String,
+    pub title: String,
+    pub description: String,
+    pub display_categories: Option<Vec<String>>,
+    pub versions: Vec<String>,
+    pub follows: i32,
+    pub date_created: String,
+    pub latest_version: Option<String>,
+    pub license: String,
+    pub gallery: Option<Vec<String>>,
+    pub featured_gallery: Option<String>,
+}
+
+impl Hit {
+    pub fn is_mod(&self) -> bool {
+        self.project_type == "mod"
+    }
+    pub fn name(&self) -> String {
+        self.slug.to_owned()
+    }
+    pub fn is_support_game_version(&self, version: &str) -> bool {
+        self.versions.iter().any(|_version| _version == version)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Projects {
+    pub hits: Vec<Hit>,
+    pub offset: i32,
+    pub limit: i32,
+    pub total_hits: i32,
+}
+
+impl Projects {
+    /// Fetches the list of Projects for a project with the given slug and limit.
+    /// # Examples
+    /// ```
+    /// use modrinth_api::Projects;
+    /// let projects = Projects::fetch_blocking("fabric-api", Some(10)).unwrap();
+    /// assert!(projects.hits.len() > 0);
+    /// ```
+    pub fn fetch_blocking(query: &str, limit: Option<usize>) -> Result<Projects> {
+        let client = Client::new();
+        let mut err_detail = None;
+        if limit.is_some_and(|lim| lim > 100) {
+            return Err(anyhow::anyhow!(
+                "limit must < 100, the limit is {:?}",
+                limit
+            ));
+        }
+        for _ in 0..5 {
+            let init = client
+                .get(format!(
+                    "https://api.modrinth.com/v2/search?query={}&limit={}",
+                    query,
+                    limit.unwrap_or(10)
+                ))
+                .header(
+                    reqwest::header::USER_AGENT,
+                    "github.com/funny233-github/MCLauncher",
+                )
+                .timeout(Duration::from_secs(100));
+
+            let send = if let Ok(_send) = init.send() {
+                _send
             } else {
+                std::thread::sleep(Duration::from_secs(3));
                 continue;
+            };
+
+            match send.json() {
+                Ok(_json) => return Ok(_json),
+                Err(e) => {
+                    err_detail = Some(e);
+                    continue;
+                }
             }
         }
 
-        Err(anyhow::anyhow!("modrinth Versions fetch timeout!"))
+        Err(anyhow::anyhow!(
+            "modrinth Projects fetch timeout!\ndetail:{:#?}",
+            &err_detail
+        ))
+    }
+
+    /// Fetches the list of Projects for a project with the given slug and limit.
+    /// # Examples
+    /// ```
+    /// use modrinth_api::Projects;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let projects = Projects::fetch("fabric-api", Some(10)).await.unwrap();
+    ///     assert!(projects.hits.len() > 0);
+    /// }
+    /// ```
+    pub async fn fetch(query: &str, limit: Option<usize>) -> Result<Projects> {
+        let client = reqwest::Client::new();
+        let mut err_detail = None;
+        if limit.is_some_and(|lim| lim > 100) {
+            return Err(anyhow::anyhow!(
+                "limit must < 100, the limit is {:?}",
+                limit
+            ));
+        }
+        for _ in 0..5 {
+            let init = client
+                .get(format!(
+                    "https://api.modrinth.com/v2/search?query={}&limit={}",
+                    query,
+                    limit.unwrap_or(10)
+                ))
+                .header(
+                    reqwest::header::USER_AGENT,
+                    "github.com/funny233-github/MCLauncher",
+                )
+                .timeout(Duration::from_secs(100));
+
+            let send = if let Ok(_send) = init.send().await {
+                _send
+            } else {
+                tokio::time::sleep(Duration::from_secs(3)).await;
+                continue;
+            };
+
+            match send.json().await {
+                Ok(_json) => return Ok(_json),
+                Err(e) => {
+                    err_detail = Some(e);
+                    continue;
+                }
+            }
+        }
+
+        Err(anyhow::anyhow!(
+            "modrinth Projects fetch timeout!\ndetail:{:#?}",
+            &err_detail
+        ))
     }
 }
